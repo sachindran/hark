f6c7d0164e52e71d8345f8b92c1f3fb13ec29f2b
Sachindran Selvaraj <sachin@Bhaskars-MacBook-Pro.local>
2015-03-09 15:56:23 -0500

    Initial Commit

---
+++ hark/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hark
+//
+//  Created by Sachindran Selvaraj on 3/9/15.
+//  Copyright (c) 2015 Sachindran Selvaraj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
